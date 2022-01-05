b36932ceeafa91208705a07c067f60a8d99a3387
Jean M. Lescure <jeanmlescure@gmail.com>
2022-01-05 11:46:42 -0600

    chore: build dist for release

--- dist/function-queue.js
+++ dist/function-queue.js
@@ -1,5 +1,7 @@
 var FunctionQueue = (() => {
   var __defProp = Object.defineProperty;
+  var __defProps = Object.defineProperties;
+  var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
   var __getOwnPropSymbols = Object.getOwnPropertySymbols;
   var __hasOwnProp = Object.prototype.hasOwnProperty;
   var __propIsEnum = Object.prototype.propertyIsEnumerable;
@@ -15,6 +17,7 @@ var FunctionQueue = (() => {
       }
     return a;
   };
+  var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
   var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
   var __export = (target, all) => {
     __markAsModule(target);
@@ -30,7 +33,7 @@ var FunctionQueue = (() => {
   });
 
   // package.json
-  var version = "1.1.0";
+  var version = "1.1.1";
 
   // src/index.ts
   var defaultOptions = {
@@ -46,17 +49,25 @@ var FunctionQueue = (() => {
   var FunctionQueue = class {
     constructor(fn, options) {
       this._queue = [];
-      this._tryFn = async (payload, retries = 0) => {
-        try {
-          await sleep(this._options.waitTimeBetweenRuns);
-          const result = await this._fn(payload);
-          return result;
-        } catch (error) {
-          if (retries < this._options.maxRetries) {
-            return await this._tryFn(payload, retries + 1);
+      this._tryFn = async (payload) => {
+        let retries = 0;
+        let result;
+        while ((!result || result.error) && retries <= this._options.maxRetries) {
+          retries++;
+          try {
+            await sleep(this._options.waitTimeBetweenRuns);
+            result = {
+              duration: 0,
+              result: await this._fn(payload)
+            };
+          } catch (error) {
+            result = {
+              duration: 0,
+              error
+            };
           }
-          throw error;
         }
+        return result;
       };
       this._fn = fn;
       this._options = __spreadValues(__spreadValues({}, defaultOptions), options || {});
@@ -74,10 +85,9 @@ var FunctionQueue = (() => {
         try {
           const result = await this._tryFn(payload);
           endTime = Date.now();
-          results.push({
-            duration: endTime - startTime,
-            result
-          });
+          results.push(__spreadProps(__spreadValues({}, result), {
+            duration: endTime - startTime
+          }));
         } catch (error) {
           endTime = Date.now();
           results.push({
@@ -93,17 +103,25 @@ var FunctionQueue = (() => {
   var FunctionSyncQueue = class {
     constructor(fn, options) {
       this._queue = [];
-      this._tryFn = (payload, retries = 0) => {
-        try {
-          syncSleep(this._options.waitTimeBetweenRuns);
-          const result = this._fn(payload);
-          return result;
-        } catch (error) {
-          if (retries < this._options.maxRetries) {
-            return this._tryFn(payload, retries + 1);
+      this._tryFn = (payload) => {
+        let retries = 0;
+        let result;
+        while ((!result || result.error) && retries <= this._options.maxRetries) {
+          retries++;
+          try {
+            syncSleep(this._options.waitTimeBetweenRuns);
+            result = {
+              duration: 0,
+              result: this._fn(payload)
+            };
+          } catch (error) {
+            result = {
+              duration: 0,
+              error
+            };
           }
-          return error;
         }
+        return result;
       };
       this._fn = fn;
       this._options = __spreadValues(__spreadValues({}, defaultOptions), options || {});
@@ -121,10 +139,9 @@ var FunctionQueue = (() => {
         try {
           const result = this._tryFn(payload);
           endTime = Date.now();
-          results.push({
-            duration: endTime - startTime,
-            result
-          });
+          results.push(__spreadProps(__spreadValues({}, result), {
+            duration: endTime - startTime
+          }));
         } catch (error) {
           endTime = Date.now();
           results.push({
